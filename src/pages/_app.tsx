ad2db6b2676c0db91ee9266b444768befeac4e1a
Alice Grandjean <alice.grandj.49@gmail.com>
2025-02-01 11:05:52 +0100

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/global.css';
 import { AppProps } from 'next/app';
 import { Roboto } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/next';
 
 const roboto = Roboto({ subsets: ['latin'], weight: '400' });
 
@@ -8,6 +9,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <div className={roboto.className}>
       <Component {...pageProps} />
+      <Analytics />
     </div>
   )
 }
